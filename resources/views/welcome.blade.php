dd20ff517f2a66ad43ef6ed1b9b45228e2503bba
gabilujan <gabriela.santos1005@etec.sp.gov.br>
2024-05-08 11:36:41 -0300

    tela login simples

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="icone.png">
+    <title>Login</title>
+
+
+
+<body>
+<form name="cliente" method = "POST" action = "">
+
+    <div class="main-login">
+        <div class="left-login">
+            <h1> Faça o login para acessar </h1>
+            <img src="signup.svg" class="left-login-image" alt="imagem animada">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Login</h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="">
+                </div>
+                <input type="submit" class="btn-login" name = "btnentrar" value = "Entrar">  
+            </div>
+        </div>
+</form>
+
+</body>
+
+</html>
+
